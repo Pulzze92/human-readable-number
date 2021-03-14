af6be63d7072dd0191b0f6225a399f145c0f1073
Pulzze92 <89998310392@mail.ru>
2021-03-15 00:11:34 +0300

    fix: repair function

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ module.exports = function toReadable (number) {
 	  [16, 'sixteen'],
 	  [17, 'seventeen'],
 	  [18, 'eighteen'],
-	  [19, 'thirteen'],
+	  [19, 'nineteen'],
 	  [20, 'twenty'],
 	  [30, 'thirty'],
 	  [40, 'forty'],
@@ -30,28 +30,42 @@ module.exports = function toReadable (number) {
 	  [90, 'ninety'],
 	  [100, 'hundreed']	
   ]);
-	 if (number <= 20) {
+	 if (number <= 20 && number !== 0) {
 	numberOut = map.get(number);
- 	} 
- 		else if (number > 20 && number <= 99) 
+ 	} else if (number == 0) {
+	numberOut = 'zero';
+}
+ 		else if (number > 20 && number <= 99 && number % 10 !== 0) 
  		{
 	 		let a = Math.floor(number / 10) * 10;
 	 		let b = number - a;
 	 		numberOut = map.get(a) + ' ' + map.get(b);
  		}
+		else if (number > 20 && number <= 99 && number % 10 == 0) {
+			numberOut = map.get(number);
+		}
  		else if (number >= 100 && number <= 999) 
  		{
 	 		let a = Math.floor(number / 100); // even hundreds
-	 		let b = (Math.floor((number - (Math.floor(number / 100) * 100)) / 10) * 10); // even dozens
-			let c = ((number - (Math.floor(number / 100)) * 100) - (Math.floor((number - (Math.floor(number / 100) * 100)) / 10) * 10)); // units
-			let d = (number - (Math.floor(number / 100)) * 100); // gross dozens
-	 		if (d % 10 == 0) { 
+	 		let b = Math.floor(number % 100 / 10) * 10; // even dozens
+			let c = number - (a * 100) - b; // units
+			let d = number - (a * 100); // gross dozens
+			console.log(a);
+			console.log(b);
+			console.log(c);
+			console.log(d);
+	 		if (d % 10 === 0 && number % 100 !== 0) {
+			console.log('krugloye'); 
 			numberOut = map.get(a) + ' ' + 'hundred' + ' ' + map.get(d);
-			} 	else if (b <= 20)	{
-				numberOut = map.get(a) + ' hundred ' + map.get(d);
-			} 
+			} else if (d <= 20 && number % 100 !== 0)	{
+			console.log('do 20');
+			numberOut = map.get(a) + ' hundred ' + map.get(d);
+			} else if (number % 100 === 0) {
+			console.log('kratno 100');
+			numberOut = map.get(a) + ' ' + 'hundred';
+			} else {
 			numberOut = map.get(a) + ' ' + 'hundred' + ' ' + map.get(b) + ' ' + map.get(c);
-			
-}
+			}
+		}
 return numberOut;
 }
